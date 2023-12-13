9fc9309a16ff5f95e7cf798e783a5bb70e7076e3
Ryota Itagaki <ribitamana@gmail.com>
2023-12-13 22:21:38 +0900

    microCMSクライアント導入

---
+++ types/service/categoryContent.ts
@@ -0,0 +1,10 @@
+type CategoryContent = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
+};
+
+export default CategoryContent;
---
+++ types/service/content.ts
@@ -0,0 +1,24 @@
+type Content = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  content: string;
+  eyecatch: {
+    url: string;
+    height: number;
+    width: number;
+  };
+  category: {
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    revisedAt: string;
+    name: string;
+  };
+};
+
+export default Content;
